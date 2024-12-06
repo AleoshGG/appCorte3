20155abe5555f0ecb49c56714c9fd0dde57dee46
Alexis Guzman <spartanthelexvro@gmail.com>
2024-12-06 12:34:50 -0600

    Modificando router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewCardsComponent } from './characters/view-cards/view-cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'characters',
+    component: ViewCardsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
